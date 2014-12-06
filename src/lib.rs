0182d78e7515d5f63ad411b5b0d3e6cc08faecb7
Tony Kovanen <tonykovanen@hotmail.com>
2014-12-06 22:25:09 +0200

    add http request structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,47 @@
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 use std::str::from_utf8;
+use std::collections::HashMap;
+
+pub enum RequestType {
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    TRACE,
+    OPTIONS,
+    CONNECT,
+    PATCH
+}
+
+#[deriving(PartialEq, PartialOrd)]
+pub enum Version {
+    HTTP_09,
+    HTTP_10,
+    HTTP_11,
+    HTTP_20
+}
+
+pub struct Request {
+    pub method: RequestType,
+    pub version: Version,
+    pub resource: String,
+    pub headers: HashMap<String, String>,
+    pub body: String
+}
+
+// Tokens
+pub const CR: u8 = b'\r';
+pub const LF: u8 = b'\n';
+pub const SP: u8 = b' ';
+pub const EOL: &'static [u8] = &[CR, LF];
+
+fn read_request(stream: TcpStream) {
+    // Read first line
+    // Read header fields
+    // Read body
+}
 
 #[test]
 fn it_works() {
@@ -33,12 +74,13 @@ fn it_works() {
         }
     });
 
-    let mut stream = TcpStream::connect("127.0.0.1:3000");
-    stream.write(b"Hello World!\r\n").unwrap();
-    println!("aoeaoeaoe");
+    spawn(proc() {
+        let mut stream = TcpStream::connect("127.0.0.1:3000");
+        stream.write(b"Hello World!\r\n").unwrap();
 
-    let mut buf = [0u8, ..4096];
-    let count = stream.read(&mut buf);
-    let msg = from_utf8(&buf).unwrap_or("");
-    println!("client got: {}", msg);
+        let mut buf = [0u8, ..4096];
+        let count = stream.read(&mut buf);
+        let msg = from_utf8(&buf).unwrap_or("");
+        println!("Client got: {}", msg);
+    });
 }
