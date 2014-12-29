fd12c7a6b5d8b6820d4c34b19d704a3132a349a4
Tony Kovanen <tonykovanen@hotmail.com>
2014-12-29 17:45:04 +0200

    make it compile with nightly

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate epic;
+
+use std::io::{TcpListener, TcpStream};
+use std::io::{Acceptor, Listener};
+use std::str::from_utf8;
+use std::collections::HashMap;
+
+fn main() {
+    let tcp_listener = TcpListener::bind("127.0.0.1:3000");
+    let mut acceptor = tcp_listener.listen();
+
+    // Spawn HTTP server
+    spawn(move || {
+        for mut opt_stream in acceptor.incoming() {
+            match opt_stream {
+                Err(e) => println!("Error: {}", e),
+                Ok(mut stream) => spawn(move || {
+                    let req = epic::HttpParser::read_request(&mut stream);
+                    println!("Req: {}", req);
+
+                    // Write something back
+                    stream.write(b"HTTP/1.1 200 VERY OK\r\nContent-Type: text/plain\r\nContent-Length:12\r\n\r\nHello");
+                    stream.write(b" world!");
+                })
+            }
+        }
+    });
+
+    // Spawn client
+    spawn(move || {
+        let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+        stream.write(b"GET /index.html HTTP/1.1\r\nContent-Type: text/plain\r\nContent-Length:12\r\nTransfer-Encoding: gzip, chunked\r\n\r\nHello").unwrap();
+        stream.write(b" world!").unwrap();
+        println!("Client got: {}", epic::HttpParser::read_response(&mut stream));
+    });
+}
